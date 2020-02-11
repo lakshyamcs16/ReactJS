990265bead0518e1c2b2ee6a551aaa17d018407f
lakshyamcs16 <sethi.laskhya94@gmail.com>
2020-02-11 19:13:01 +0530

    removed node modules and added lyrics + placeholders

--- YouTubePlayer/youtube-player-share/src/Components/video_detail.js
+++ YouTubePlayer/youtube-player-share/src/Components/video_detail.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React,  { useState, useEffect } from 'react';
+const lyrics = require('4lyrics');
+
 
 const VideoDetail = (props) => {
     const video = props.video;
+    const [lyric, setLyric] = useState({lyrics: null});
+
+    useEffect(()=>{
+        lyrics.lyricslive.getURL('up&up')
+            .then(r => lyrics.lyricslive.getLyrics(r))
+            .then((r) => {
+                setLyric({
+                    lyrics: r
+                })
+            })
+            .catch(console.error);
+    })
 
     if(!video) {
         return <div>Loading. . . </div>
@@ -14,6 +28,7 @@ const VideoDetail = (props) => {
             <div className="details">
                 <div>{video.snippet.title}</div>
                 <div>{video.snippet.description}</div>
+                <div>{lyric.lyrics}</div>
             </div>
         </div>
     );
