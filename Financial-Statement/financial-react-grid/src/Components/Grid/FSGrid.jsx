1b47543fe1bb09de69bb10987a040d7744fcff56
lakshyamcs16 <sethi.laskhya94@gmail.com>
2020-02-19 19:38:00 +0530

    Sum at tree level

--- Financial-Statement/financial-react-grid/src/Components/Grid/FSGrid.jsx
+++ Financial-Statement/financial-react-grid/src/Components/Grid/FSGrid.jsx
@@ -12,7 +12,9 @@ import {
     SearchState,
     IntegratedFiltering,
     FilteringState,
-    DataTypeProvider
+    DataTypeProvider,
+    SummaryState,
+    IntegratedSummary
 } from '@devexpress/dx-react-grid';
 import {
     Grid,
@@ -22,6 +24,7 @@ import {
     TableTreeColumn,
     TableSelection,
     TableColumnResizing,
+    TableSummaryRow,
     SearchPanel,
     DragDropProvider,
     TableColumnReordering,
@@ -38,21 +41,38 @@ const getChildRows = (row, rootRows) => {
 const compareNumbers = (a, b) => {
     return a - b;
 }
+
+const CurrencyFormatter = ({ value }) => `${value}`;
+
+const CurrencyTypeProvider = props => (
+  <DataTypeProvider
+    formatterComponent={CurrencyFormatter}
+    {...props}
+  />
+);
+
 class FSGrid extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             rows: [],
-            columns: [],
+            columns: [
+                { name: 'Name' },
+                { name: 'FY 2015' },
+                { name: 'FY 2016' },
+                { name: 'FY 2017' },
+                { name: 'FY 2018' },
+                { name: 'FY 2019' }
+            ],
             selection: [],
             tableColumnExtensions: [{ columnName: 'Name', width: 300 }],
             defaultColumnWidths: [{ columnName: 'Name', width: 300 },
-            { columnName: 'FY 2015', width: 200 },
-            { columnName: 'FY 2016', width: 200 },
-            { columnName: 'FY 2017', width: 200 },
-            { columnName: 'FY 2018', width: 200 },
-            { columnName: 'FY 2019', width: 200 }
+                                { columnName: 'FY 2015', width: 200 },
+                                { columnName: 'FY 2016', width: 200 },
+                                { columnName: 'FY 2017', width: 200 },
+                                { columnName: 'FY 2018', width: 200 },
+                                { columnName: 'FY 2019', width: 200 }
             ],
             integratedSortingColumnExtensions: [{ columnName: 'FY 2015', compare: compareNumbers }],
             defaultColumnOrder: ['Name', 'FY 2015', 'FY 2016', 'FY 2017', 'FY 2018', 'FY 2019'],
@@ -65,6 +85,8 @@ class FSGrid extends Component {
                 'lessThan',
                 'lessThanOrEqual',
             ],
+            totalSummaryItems: [ {columnName: 'FY 2015', type: 'sum' }],
+            treeSummaryItems: [ {columnName: 'FY 2015', type: 'sum' }],
             pageSizes: [5, 10, 15, 0],
             defaultExpandedRowIds: [0],
             searchValue: ''
@@ -83,20 +105,32 @@ class FSGrid extends Component {
         })
     }
 
+    parseData = (data) => {
+        debugger;
+        return data.map((v) => {
+            var currObj = {};
+            for(var key in v) {
+                if(key.toUpperCase().startsWith("FY")) {
+                    var vv = 0;
+                    if(!isNaN(v[key])) {
+                        vv = parseInt(v[key]);
+                    }
+                    currObj[key] = vv;
+                }else{
+                    currObj[key] = v[key];
+                }
+            }
+            return currObj;
+        })
+    }
+
     componentDidMount() {
         fetch(URL)
             .then(e => e.json())
             .then(res => {
                 this.setState({
-                    rows: res.documents[0].data,
-                    columns: [
-                        { name: 'Name' },
-                        { name: 'FY 2015' },
-                        { name: 'FY 2016' },
-                        { name: 'FY 2017' },
-                        { name: 'FY 2018' },
-                        { name: 'FY 2019' }
-                    ]
+                    rows: this.parseData(res.documents[0].data),
+                    columns: this.state.columns
                 });
             })
     }
@@ -109,6 +143,9 @@ class FSGrid extends Component {
                         rows={this.state.rows}
                         columns={this.state.columns}
                     >
+                        <CurrencyTypeProvider
+                            for={this.state.currencyColumns}
+                        />
                         <SortingState
                             defaultSorting={[{ columnName: 'Name', direction: 'asc' }]}
                         />
@@ -118,6 +155,10 @@ class FSGrid extends Component {
                             onValueChange={this.setSearchValue}
                         />
                         <TreeDataState defaultExpandedRowIds={this.state.defaultExpandedRowIds} />
+                        <SummaryState
+                            totalItems={this.state.totalSummaryItems}
+                            treeItems={this.state.treeSummaryItems}
+                        />
                         <CustomTreeData
                             getChildRows={getChildRows}
                         />
@@ -147,6 +188,7 @@ class FSGrid extends Component {
                         />
                         <TableColumnReordering defaultOrder={this.state.defaultColumnOrder} />
                         <TableColumnResizing defaultColumnWidths={this.state.defaultColumnWidths} />
+                        <IntegratedSummary />
                         <TableHeaderRow showSortingControls />
                         <TableFilterRow
                             showFilterSelector
@@ -157,6 +199,7 @@ class FSGrid extends Component {
                         <TableTreeColumn
                             for="Name"
                         />
+                        <TableSummaryRow />
 
                     </Grid>
                 </Paper>
@@ -166,34 +209,3 @@ class FSGrid extends Component {
 }
 
 export default FSGrid;
-
-
-
-
-
-/*
-
-Sample tree data
-
-0: {id: 0, parentId: null, gender: "Female", name: "Barbara", city: "Tokyo", …}
-1: {id: 1, parentId: 0, gender: "Female", name: "Betty", city: "Chicago", …}
-2: {id: 2, parentId: 0, gender: "Male", name: "Thomas", city: "New York", …}
-3: {id: 3, parentId: 0, gender: "Male", name: "John", city: "Paris", …}
-4: {id: 4, parentId: 0, gender: "Female", name: "Maria", city: "Las Vegas", …}
-5: {id: 5, parentId: 0, gender: "Male", name: "John", city: "Austin", …}
-6: {id: 6, parentId: 0, gender: "Male", name: "Richard", city: "Las Vegas", …}
-7: {id: 7, parentId: 1, gender: "Male", name: "William", city: "New York", …}
-8: {id: 8, parentId: 2, gender: "Female", name: "Maria", city: "Los Angeles", …}
-9: {id: 9, parentId: 1, gender: "Male", name: "James", city: "Chicago", …}
-10: {id: 10, parentId: 3, gender: "Female", name: "Maria", city: "Los Angeles", …}
-11: {id: 11, parentId: 1, gender: "Female", name: "Sharon", city: "Tokyo", …}
-12: {id: 12, parentId: 1, gender: "Male", name: "William", city: "London", …}
-13: {id: 13, parentId: 0, gender: "Male", name: "John", city: "New York", …}
-14: {id: 14, parentId: 0, gender: "Male", name: "David", city: "Tokyo", …}
-15: {id: 15, parentId: 1, gender: "Female", name: "Sandra", city: "Tokyo", …}
-16: {id: 16, parentId: 6, gender: "Male", name: "John", city: "New York", …}
-17: {id: 17, parentId: 0, gender: "Male", name: "John", city: "Paris", …}
-18: {id: 18, parentId: 2, gender: "Male", name: "William", city: "Paris", …}
-19: {id: 19, parentId: 8, gender: "Male", name: "David", city: "Las Vegas", …}
-
-*/
