b128a70491babdfe6538ba5032c3b50ae7cc5229
Lakshya Sethi <lakshyasethi@Lakshyas-MacBook-Pro.local>
2021-07-18 22:34:31 +0530

    layout

--- infinite-scroll/src/components/FlatList.tsx
+++ infinite-scroll/src/components/FlatList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {IFlatListState, IFlatList} from '../interface/IFlatList';
+import { IFlatListState, IFlatList } from "../interface/IFlatList";
 
 export default class FlatList extends React.Component<
   IFlatList,
@@ -22,7 +22,14 @@ export default class FlatList extends React.Component<
   loadMore = (e: Event) => {
     if (
       this.props.callback &&
-      (window.innerHeight + window.pageYOffset) >= document.body.scrollHeight - 2
+      navigator.userAgent.indexOf("Safari") !== -1 &&
+      window.innerHeight + document.documentElement.scrollTop === document.scrollingElement?.scrollHeight
+    ) {
+      this.state.scrollCallback?.(e);
+    } else if (
+      this.props.callback &&
+      navigator.userAgent.indexOf("Safari") === -1 &&
+      window.innerHeight + window.pageYOffset >= document.body.scrollHeight - 2
     ) {
       this.state.scrollCallback?.(e);
     }
@@ -30,13 +37,17 @@ export default class FlatList extends React.Component<
 
   render() {
     return (
-      <div className={this.props.horizontal? "d-flex" : "item-container"}>
+      <div className={this.props.horizontal ? "d-flex" : "item-container"}>
         {this.props.data.map((item, index) => {
           return (
             <div
               key={this.props.keyExtractor(item) || index}
               data-id={this.props.keyExtractor(item) || index + 1}
-              className={Array.isArray(this.props.classes)? this.props.classes.join(" ") : this.props.classes}
+              className={
+                Array.isArray(this.props.classes)
+                  ? this.props.classes.join(" ")
+                  : this.props.classes
+              }
             >
               {this.props.render(item, index)}
             </div>
